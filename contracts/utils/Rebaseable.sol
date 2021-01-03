1a78063854ae190815e6c9f5c5ddf95733931e64
stableflect.finance <admin@stableflect.finance>
2021-01-03 12:51:52 +0100

    Add abstract Rebaseable contract

---
+++ contracts/utils/Rebaseable.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier:
+/*
+ *
+ */
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+/**
+ * @dev Interface of 'rebase able' coin type.
+ */
+abstract contract Rebaseable {
+
+    // Used for authentication
+    address private _monetaryPolicy;
+
+    /**
+     * @dev Modifier limits rebase functions access to monetary policy contract.
+     */
+    modifier onlyMonetaryPolicy() {
+        require(msg.sender == _monetaryPolicy);
+        _;
+    }
+
+    /**
+    * @dev Sets monetary policy contract address.
+    * @param monetaryPolicy_ The address of the monetary policy contract to use for authentication.
+    *
+    * Note this function should be limited to onlyOwner.
+    */
+    function _setMonetaryPolicy(address monetaryPolicy_) internal virtual {
+        _monetaryPolicy = monetaryPolicy_;
+    }
+
+    /**
+     * @dev Notifies STAB contract about a new rebase cycle.
+     * @param epoch The next epoch number.
+     * @param exchangeRate Current STAB exchange rate.this
+     * @param targetRate STAB price rebase target.
+     * @param rebaseLag Rebase lag when rebaseLag > 1 or rebase leverage (when rebase lag < -1).
+     * @return The total number of tokens after the supply adjustment.
+     */
+    function rebase(uint256 epoch, uint256 exchangeRate, uint256 targetRate, int256 rebaseLag) external virtual returns (uint256);
+
+    /**
+    * @dev Emitted when rebase started.
+    */
+    event LogRebase(uint256 indexed epoch, uint256 totalSupply);
+
+    /**
+    * @dev Emitted when monetary policy address changed.
+    *
+    * Note only monetary policy have access to rebase functionality.
+    */
+    event LogMonetaryPolicyUpdated(address monetaryPolicy);
+}
